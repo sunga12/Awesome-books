0f0b732fb40e60b6dcdf4cf0513e63b5153c7d1a
til2to <padwusup@gmail.com>
2023-04-24 17:12:27 +0000

    Add and remove book

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-// create the page setup withhtml
-// title h1
-// initial empty list + add book form
-// add book form: short inputs x2 button x1
-// placeholders Title, Author, Add
-
 const books = document.querySelector('.books');
 const form = document.querySelector('.form');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 
 let localStorage = JSON.parse(window.localStorage.getItem('allBooks'));
-
-// create array []
-
 if (localStorage === null || localStorage === undefined || localStorage.length === 0) {
   localStorage = [];
 }
 
+// create array []
 let allBooks = localStorage;
 
 allBooks.forEach((book, index) => {
@@ -34,3 +26,45 @@ allBooks.forEach((book, index) => {
 const bookExist = (existingTitle,
   newTitle) => JSON.stringify(existingTitle) === JSON.stringify(newTitle);
 
+/* add book */
+const addBook = () => {
+  const newBook = {
+    title: title.value,
+    author: author.value,
+  };
+  /* check if book exist */
+  let exist = false;
+  localStorage.forEach((book) => {
+    if (bookExist(book.title, title.value)) {
+      exist = true;
+    }
+  });
+
+  /* Dont add if book exist */
+  if (exist) return;
+
+  /* add book if it doesn't exist */
+  allBooks.unshift(newBook);
+  window.localStorage.setItem('allBooks', JSON.stringify(localStorage));
+  window.location.reload();
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBook();
+});
+
+/* remove book */
+function remove(buttonId) {
+  allBooks = localStorage.filter((book, index) => index !== buttonId);
+  window.localStorage.setItem('allBooks', JSON.stringify(allBooks));
+  window.location.reload();
+}
+
+const bookBtns = document.querySelectorAll('.remove');
+bookBtns.forEach((bookBtn) => {
+  bookBtn.addEventListener('click', (e) => {
+    const buttonId = parseInt(e.target.getAttribute('id'), 10);
+    remove(buttonId);
+  });
+});
